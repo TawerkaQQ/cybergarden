6302bd1b80897b0ae57567a2a96a6a9883b0d6d1
Ivan <ivkalinin@sfedu.ru>
2023-12-23 13:55:56 +0300

    add full name det

--- MainTest.py
+++ MainTest.py
@@ -5,7 +5,7 @@ from transformers import AutoTokenizer, AutoModel
 # model='cointegrated/rubert-tiny2',
 # tokenizer= 'cointegrated/rubert-tiny2'
 
-anonym = pdfAnonymizer(path_to_pdf = "/home/tawerka/projects/cybergarden/testJPG.jpg",
+anonym = pdfAnonymizer(path_to_pdf = "/home/tawerka/projects/cybergarden/testPDF.pdf",
                        # model='ai-forever/ruBert-base',
                        # tokenizer= 'ai-forever/ruBert-base'
                        )
@@ -14,7 +14,7 @@ anonym = pdfAnonymizer(path_to_pdf = "/home/tawerka/projects/cybergarden/testJPG
 # Calling the generic function
 anonym.anonymize(output_path = './deoutput13rubert-base.pdf',
                      remove_metadata = True,
-                     fill = 'black',
-                     outline = 'black',
+                     fill = 'green',
+                     outline = 'green',
                     )
 
